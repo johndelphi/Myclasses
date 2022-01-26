fb747d49dac3aeedb0d871a185f39985b1dd0f56
John.michael <John.michael@studentambassadors.com>
2022-01-25 20:59:13 -0600

    Add project files.

---
+++ Myclasses/Fileprocess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Myclasses
+{
+  public class Fileprocess
+  {
+    public bool Fileexists(string fileName)
+    {
+      if (string.IsNullOrEmpty(fileName))
+      {
+        throw new
+          ArgumentNullException("fileName");
+      }
+      return File.Exists(fileName);
+    }
+  }
+}
